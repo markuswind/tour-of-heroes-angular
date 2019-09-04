5309cff2aa3550b9a6c11a33d90a9a8e596ec9bf
Markus Wind <markuswind@gmail.com>
2019-09-04 13:31:16 +0200

    feat: completed routing step

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,11 @@ import { MessageService } from './message.service';
 export class HeroService {
   constructor(private messageService: MessageService) {}
 
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero with id: ${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
